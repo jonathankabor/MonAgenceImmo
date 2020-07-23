cd53ceb90e0a1481f90ef566150b28a4e7c6cb5e
kabor <kabor@LAPTOP-1M107DHK.(none)>
2020-07-23 12:52:18 +0200

    création de fixtures

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -90,7 +90,7 @@ class User implements  UserInterface, \Serializable
     public function serialize()
     {
         // TODO: Implement serialize() method.
-        return $this->serialize([
+        return serialize([
             $this->id,
             $this->username,
             $this->password
