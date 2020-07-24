0485353453cd2be07409b458a8f31ca562a48357
kabor <kabor@LAPTOP-1M107DHK.(none)>
2020-07-25 01:35:15 +0200

    css mise en forme des formulaires

--- src/Entity/PropertySearch.php
+++ src/Entity/PropertySearch.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 class PropertySearch
 {
@@ -12,6 +14,7 @@ class PropertySearch
 
     /**
      * @var int|null
+     * @Assert\Range(min=10, max=500)
      */
     private $minSurface;
 
