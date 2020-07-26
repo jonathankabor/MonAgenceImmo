0448da3aacca31920e75afb8ced5669f4ed9a12d
kabor <kabor@LAPTOP-1M107DHK.(none)>
2020-07-26 11:45:49 +0200

    ajout du champ image

--- migrations/Version20200726090636.php
+++ migrations/Version20200726090636.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200726080922 extends AbstractMigration
+final class Version20200726090636 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20200726080922 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE property ADD filename VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE property ADD updated_at DATETIME NOT NULL, ADD update_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE property DROP filename');
+        $this->addSql('ALTER TABLE property DROP updated_at, DROP uploaded_at');
     }
 }
--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -7,9 +7,9 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
-use Faker\Provider\File;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 
@@ -352,9 +352,9 @@ class Property
     }
 
     /**
-     * @return null|File
+     * @return File|null
      */
-    public function getImageFile(): ?File
+    public function getImageFile()
     {
         return $this->imageFile;
     }
@@ -368,4 +368,5 @@ class Property
         $this->imageFile = $imageFile;
         return $this;
     }
+
 }
