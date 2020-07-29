92234a017d82abb93c9f53d1365a93ab7627493c
kabor <kabor@LAPTOP-1M107DHK.(none)>
2020-07-29 03:00:22 +0200

    suppression image

--- src/Controller/Admin/AdminPictureController.php
+++ src/Controller/Admin/AdminPictureController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Picture;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,17 +18,23 @@ class AdminPictureController extends  AbstractController
 
     /**
      * @Route("/{id}", name="admin.picture.delete", methods="DELETE")
+     * @param Picture $picture
      */
     public function delete(Picture $picture, Request $request)
     {
         $propertyId = $picture->getProperty()->getId();
-        if($this->isCsrfTokenValid('delete' . $picture->getId(), $request->request->get('_token'))){
+        //$data = json_decode($request->getContent(), true);
+        if ($this->isCsrfTokenValid('delete' . $picture->getId(), $request->request->get('_token'))) //if ($this->isCsrfTokenValid('delete' . $picture->getId(), $data['_token']))
+        {
             $manager = $this->getDoctrine()->getManager();
             $manager->remove($picture);
             $manager->flush();
+            //return new JsonResponse(['success' => 1]);
         }
+        return $this->redirectToRoute('admin.property.edit', ['id'=> $propertyId]);
 
-        return$this->redirectToRoute('admin.property.edit', ['id' => $propertyId]);
+        //return new JsonResponse(['error' => 'Token invalid'], 400);
+
 
     }
 }
--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -8,11 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Cocur\Slugify\Slugify;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\HttpFoundation\File\File;
-
-use Symfony\Component\Validator\Constraints\Image;
 
 
 /**
